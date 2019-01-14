d5a07dbc1d0eec2c10d3e2539acf8df190a747ff
Panya Farnette <naienko@gmail.com>
2019-01-14 11:52:07 -0600

    set up html structure and reader

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,15 +1,38 @@
-/*
-    Define the keys and value for a JavaScript object that
-    represents a journal entry about what you learned today
-*/
+const journalEntries = [
+    {
+        date: "01/10/2019",
+        concepts: "Objects and notations",
+        entry: "today we learned about objects, dot notation, and bracket notation",
+        mood: "happy"
+    },
+    {
+        date: "01/08/2019",
+        concepts: "group project",
+        entry: "I should not have lied about how the group project made me feel",
+        mood: "frustrated"
+    }
+];
 
-const allEntries = [];
-
-const objectsJournalEntry = {
-    date: 01/10/2019,
-    title: "Objects and notations",
-    entry: "today we learned about objects, dot notation, and bracket notation",
-    mood: "happy"
+const makeJournalEntryComponent = (journalEntry) => {
+    // Create your own HTML structure for a journal entry
+    return `<hr /><header>${journalEntry.concepts}</header>
+    <span class="date">${journalEntry.date}</span>
+    <section class="entry">
+        <p>${journalEntry.entry}</p>
+        <p>Mood: ${journalEntry.mood}</p>
+    </section>
+    `;
 }
 
-allEntries.push(objectsJournalEntry);
+const renderJournalEntries = (entries) => {
+    let HTMLsquirt = "";
+    for (let i = 0; i < entries.length; i++) {
+        const element = entries[i];
+        HTMLsquirt += makeJournalEntryComponent(element);
+    }
+    let location = document.querySelector(".entryLog");
+    location.innerHTML = HTMLsquirt;
+}
+
+// Invoke the render function
+renderJournalEntries(journalEntries);
