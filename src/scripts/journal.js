bc361cd2c46ca74881968e08f8c379686f5b425d
Panya Farnette <naienko@gmail.com>
2019-01-14 09:50:47 -0600

    first entry

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -2,6 +2,14 @@
     Define the keys and value for a JavaScript object that
     represents a journal entry about what you learned today
 */
-const objectsJournalEntry = {
 
+const allEntries = [];
+
+const objectsJournalEntry = {
+    date: 01/10/2019,
+    title: "Objects and notations",
+    entry: "today we learned about objects, dot notation, and bracket notation",
+    mood: "happy"
 }
+
+allEntries.push(objectsJournalEntry);
