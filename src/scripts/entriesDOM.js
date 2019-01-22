1b8c8e7a3addbfed6362de5455a629f135a0f910
Panya Farnette <naienko@gmail.com>
2019-01-22 14:19:39 -0600

    modularized, link switch between form and entries

--- src/scripts/entriesDOM.js
+++ src/scripts/entriesDOM.js
@@ -1,18 +1,26 @@
 // responsible for modifying the DOM
 
+let insertHere = document.querySelector("#display");
+
 const renderJournalEntries = (entries) => {
     let HTMLsquirt = "";
     for (let i = 0; i < entries.length; i++) {
         const element = entries[i];
         HTMLsquirt += makeJournalEntryComponent(element);
     }
-    let location = document.querySelector(".entryLog");
-    location.innerHTML = HTMLsquirt;
+    // let insertHere = document.querySelector(".entryLog");
+    insertHere.innerHTML = HTMLsquirt;
 }
 
 const renderFormElement = () => {
     let HTMLsquirt = "";
     HTMLsquirt += makeJournalFormComponent();
-    let location = document.querySelector("#formset");
-    location.innerHTML = HTMLsquirt;
+    // let insertHere = document.querySelector("#formset");
+    insertHere.innerHTML = HTMLsquirt;
 }
+
+const formClick = document.querySelector("#getform");
+formClick.addEventListener("click", renderFormElement);
+
+const entryClick = document.querySelector("#getentries");
+entryClick.addEventListener("click", showAllEntries);
