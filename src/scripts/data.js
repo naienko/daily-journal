05ee7281ccbcb4668bfbf59e98458d35b7a7a67b
Panya Farnette <naienko@gmail.com>
2019-02-12 13:19:03 -0600

    streamlining the api object

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -4,17 +4,23 @@ Intent: all contacts with the json server
 */
 
 const API = {
-    getJournalEntries () { // this is a method defined on the API object
+    get: () => { // this is a method defined on the API object
         return fetch("http://localhost:8088/entries") // fetch from the JSON
             .then(entries => entries.json()); // parse AS json
     },
-    saveJournalEntry (newJournalEntry) {
-        fetch("http://localhost:8088/entries",{
+    create: newJournalEntry => {
+        return fetch("http://localhost:8088/entries",{
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify(newJournalEntry)
-        });
+        })
+        .then(entries => entries.json()); // parse AS json
+    },
+    delete: itemId => {
+        return fetch(`http://127.0.0.1:8088/entries/${itemId}`, {
+                method: "DELETE"
+        })
     }
 };
