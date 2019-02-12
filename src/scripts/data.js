d71ac9773151908ba9df350f7d06aaa2d9792b46
Panya Farnette <naienko@gmail.com>
2019-02-12 13:23:21 -0600

    added delete and edit methods to api object in data.js

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -5,11 +5,11 @@ Intent: all contacts with the json server
 
 const API = {
     get: () => { // this is a method defined on the API object
-        return fetch("http://localhost:8088/entries") // fetch from the JSON
+        return fetch("http://localhost:8081/entries") // fetch from the JSON
             .then(entries => entries.json()); // parse AS json
     },
     create: newJournalEntry => {
-        return fetch("http://localhost:8088/entries",{
+        return fetch("http://localhost:8081/entries",{
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
@@ -19,8 +19,18 @@ const API = {
         .then(entries => entries.json()); // parse AS json
     },
     delete: itemId => {
-        return fetch(`http://127.0.0.1:8088/entries/${itemId}`, {
+        return fetch(`http://127.0.0.1:8081/entries/${itemId}`, {
                 method: "DELETE"
         })
+    },
+    edit: (journalEntryObject, itemId) => {
+        return fetch(`http://127.0.0.1:8088/contacts/${itemId}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(journalEntryObject)
+        })
+        .then(entries => entries.json()); // parse AS json
     }
 };
