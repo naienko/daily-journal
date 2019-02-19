35366ce44a99ec94568680aaeb7a6bb8cd80f134
Panya Farnette <naienko@gmail.com>
2019-02-19 15:24:18 -0600

    timestamps are interpreted as human-readable; rolling back by one day

--- src/scripts/entryComponent.js
+++ src/scripts/entryComponent.js
@@ -3,10 +3,13 @@ Author: Panya
 Intent:  responsible for creating the journal entry HTML component
 */
 
+import moment from "moment";
+
 const makeEntries = {
     createSingleEntry: journalEntry => {
+        let realDate = moment(journalEntry.date).format("dddd, MMMM Do YYYY");
         return `<header>${journalEntry.concepts}</header>
-        <span class="date">${journalEntry.date}</span>
+        <span class="date">${realDate}</span>
         <section class="entry">
             <p>${journalEntry.entry}</p>
             <p>Mood: ${journalEntry.mood.mood}</p>
