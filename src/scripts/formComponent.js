28d9fb6dc721827d934b31af642779e024fd1915
Panya Farnette <naienko@gmail.com>
2019-02-19 13:53:35 -0600

    this code creates an html string; how do I get it into the form

--- src/scripts/formComponent.js
+++ src/scripts/formComponent.js
@@ -5,17 +5,15 @@ Intent:  responsible for creating the entry form HTML component
 
 import API from "./data";
 
-const showAllMoods = () => {
-    API.get("moods")
+API.get("moods")
     .then(moodArray => {
         let HTMLcode = "";
         moodArray.forEach(element => {
             HTMLcode += `<option value=${element.id}>${element.mood}</option>
-            `
+`
         });
             return HTMLcode;
     })
-}
 
 const makeJournalFormComponent = () => {
     // Create HTML structure for the entry form
